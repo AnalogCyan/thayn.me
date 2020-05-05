39f83a99dc318e98191b2eb153a1a99a4a172294
Cyan Thayn <git@thayn.me>
2020-05-05 11:27:25 -0500

    Moved quotes, fixes #5

--- javascript.js
+++ javascript.js
@@ -1,28 +1,49 @@
 // Hey look, this is what's in the thayn.me JavaScript file!
 // If you got here from the site footer, below is a list of all the quotes that can show up there!
 
-var quotes = [
-  'Are you ready to embrace uncertainty?',
-  'Are you ready to embrace uncertainty?',
-  '<a href="https://slimegirls.bandcamp.com/album/vacation-wasteland-ep" target="_blank" rel="noopener">Please listen kindly.</a>',
-  '<a href="https://youtu.be/Yw6u6YkTgQ4" target="_blank" rel="noopener">Hello, World!</a>',
-  'Keep it simple, stupid!',
-  '<a href="./javascript.js" target="_blank">Now with JavaScript!</a>',
-  '<i class="fab fa-html5"></i> <i class="fab fa-css3-alt"></i> <i class="fab fa-js-square"></i> <i class="fab fa-font-awesome"></i>',
-  '<a href="https://en.wikipedia.org/wiki/Dieter_Rams#%22Good_design%22_principles" target="_blank" rel="noopener">Good design is...</a>',
-  'DFTBA',
-  'Don\'t forget to be awesome.',
-  'Darling, fetch the battle axe.',
-  '<a href="./.well-known/humans.txt">Run by humans (probably).</a>'
-];
+window.onload = function load() {
+  randAvatar();
+  newQuote();
+  deadPixel();
+
+  setInterval(() => {
+    randAvatar();
+  }, 2500);
+};
+
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status == 200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
+
+function randAvatar() {
+  var randomNumber = Math.floor(Math.random() * 2) + 1;
+  document.getElementById("avatar").src = `./media/${randomNumber}.jpg`;
+}
 
 function newQuote() {
-  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  var quotes = loadFile("./media/quotes.txt").split("\n");
+  var randomNumber = Math.floor(Math.random() * quotes.length);
   var randQuote = quotes[randomNumber];
-  document.getElementById('quoteDisplay').innerHTML = randQuote;
+  document.getElementById("quoteDisplay").innerHTML = randQuote;
 
-  if (randQuote.replace(/<[^>]*>/g, '').length <= 15) {
+  if (randQuote.replace(/<[^>]*>/g, "").length <= 15) {
     document.getElementById("footer-span").style.display = "flex";
-    document.getElementById("footer-span").style.justifyContent = "space-between";
+    document.getElementById("footer-span").style.justifyContent =
+      "space-between";
   }
+}
+
+// ...but I went and made it more evil!
+function deadPixel() {
+  var randomTop = Math.floor(Math.random() * window.innerHeight);
+  var randomLeft = Math.floor(Math.random() * window.innerWidth);
+  document.getElementById("broken-green-pixel").style.top = randomTop + "px";
+  document.getElementById("broken-green-pixel").style.left = randomLeft + "px";
 }
