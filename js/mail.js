bc934a3e8c13093c99e242f266cae57a40813750
AnalogCyan <git@thayn.me>
2021-09-03 13:01:54 -0500

    Moving mail redirect.

--- js/mail.js
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from "./uuid/dist/esm-browser/index.js";
-window.onload = function load() {
-  newMail();
-};
-
-function newMail() {
-  console.log(uuidv4());
-  window.location.replace("mailto:" + uuidv4() + "@mail.thayn.me");
-}
