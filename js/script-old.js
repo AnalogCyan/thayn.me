32856cdf8f57b4430e54294903a5832a74fa8cdc
AnalogCyan <git@thayn.me>
2024-07-14 20:08:14 -0500

    Re-implement site in vanilla HTML/CSS (v3-rewrite) (#31)
    * gender
    
    * Update netlify.toml
    
    * Prepare branch for rewrite.
    
    * Prepare new netlify.toml
    
    * Assign URL to branch.
    
    * Remove domain contexts for now.
    
    * Set up html & manifest templates
    
    * Add template CSS.
    
    * Add link styling
    
    * Disable cache during development.
    
    * Set up basic layout.
    
    * Update link styling and fix responsive style.
    
    * Update theme colors and add js file
    
    * Update Content-Security-Policy in netlify.toml
    
    * Update Content-Security-Policy to include Google Fonts and additional font sources
    
    * Fix margin/padding/width
    
    * Update layout variables and add transitions
    
    * Code cleanup
    
    * Update styles and rework text.
    
    * Update profile picture source
    
    * Update site icons
    
    * Update project cards and footer styles

---
+++ js/script-old.js
@@ -0,0 +1,45 @@
+// Hey look, this is what's in the thayn.me JavaScript file!
+// If you got here from the site footer, you can find a list of all the quotes that can show up there
+// at https://thayn.me/media/quotes.txt
+
+window.onload = function load() {
+  newQuote();
+  deadPixel();
+};
+
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status === 200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
+
+function randPhoto() {
+  let num = Math.floor(Math.random() * 3) + 1;
+  document.getElementById("profile").src = `./media/profile${num}.jpg`;
+}
+
+function newQuote() {
+  var quotes = loadFile("./media/quotes.txt").split("\n");
+  var randomNumber = Math.floor(Math.random() * quotes.length);
+  var randQuote = quotes[randomNumber];
+  document.getElementById("quoteDisplay").innerHTML = randQuote;
+
+  if (randQuote.replace(/<[^>]*>/g, "").length <= 15) {
+    document.getElementById("footer-span").style.display = "flex";
+    document.getElementById("footer-span").style.justifyContent =
+      "space-between";
+  }
+}
+
+// ...but I went and made it more evil!
+function deadPixel() {
+  var randomTop = Math.floor(Math.random() * window.innerHeight);
+  var randomLeft = Math.floor(Math.random() * window.innerWidth);
+  document.getElementById("broken-green-pixel").style.top = randomTop + "px";
+  document.getElementById("broken-green-pixel").style.left = randomLeft + "px";
+}
